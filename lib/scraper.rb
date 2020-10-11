a64e0db6bcc88d55d6fa802c811a937767f96e97
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-11 05:18:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
   
   def get_courses
     title = doc.css(".post").first.css("h2").text
-    schedule = 
+    schedule = doc.css(".post").first.css(".date").text
     description = 
   end
   
