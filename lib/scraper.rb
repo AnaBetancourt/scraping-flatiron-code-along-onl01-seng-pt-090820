e4f0c7a92f64636c36f0159bf8f9330ffe0f10e7
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-11 05:24:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,16 +8,11 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses'))
-    doc.css(".post").each do |post|
-      course = Course.new
-      Course.title = doc.css(".post").first.css("h2").text
-      Course.schedule = doc.css(".post").first.css(".date").text
-      Course.description = doc.css(".post").first.css("p").text
-    end
   end
   
   
   def get_courses
+    
     
   end
   
